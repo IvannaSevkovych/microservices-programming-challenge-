8c3a8e1f582117d37884ea99d697b88b60d73618
ivannaSevkovychItk <ivanna.sevkovych@itk-engineering.de>
2023-06-01 18:28:12 +0200

    Feat: add volume to postgres & adjust init.sql

--- init.sql
+++ init.sql
@@ -1,8 +1,5 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER,
     name VARCHAR(50),
     email VARCHAR(50)
 );
-
-INSERT INTO users (id, name, email)
-VALUES (1, 'John Doe', 'john@example.com');
