529df1f1284727ee2a6c2436cc9db7aea92fba35
ivannaSevkovychItk <ivanna.sevkovych@itk-engineering.de>
2023-06-03 16:52:09 +0200

    Feat: restructure get call to use db in roder service

--- order/app.py
+++ order/app.py
@@ -1,21 +1,72 @@
 from flask import Flask, jsonify, request
+import requests
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
 app = Flask(__name__)
 
-orders = [{"id": 1, "userId": 1, "productIds": [1, 2]}, {"id": 2, "userId": 2, "productIds": [2, 3]}]
-
-def findRequestedOrder(id):
-   return next((order for order in orders if order["id"] == id), None)
+# Configure the database connection:
+app.config['DATABASE'] = {
+    'dbname': os.getenv('POSTGRES_DATABASE'),
+    'user': os.getenv('POSTGRES_USER'),
+    'password': os.getenv('POSTGRES_PASSWORD'),
+    'host': os.getenv('POSTGRES_HOST'),
+    'port': os.getenv('POSTGRES_PORT')
+}
+def get_db():
+    conn = psycopg2.connect(
+        dbname=app.config['DATABASE']['dbname'],
+        user=app.config['DATABASE']['user'],
+        password=app.config['DATABASE']['password'],
+        host=app.config['DATABASE']['host'],
+        port=app.config['DATABASE']['port']
+    )
+    return conn
 
 @app.route("/orders", methods=['GET'])
 def get_orders():
-    if not orders:
-        return 'No orders found', 404
-    return jsonify(orders), 200
 
+    conn = get_db()
+    cur = conn.cursor()
+
+    # Execute an SQL query
+    cur.execute('SELECT * FROM orders')
+
+    # Fetch all rows from the result set
+    rows = cur.fetchall()
+
+    # Close the cursor
+    cur.close()
+
+    conn.close()
+
+    if not rows:
+        return 'No orders found', 404
+    return jsonify(rows), 200
+
+
+# TODO
 @app.route('/orders', methods=['POST'])
 def add_order():
     if not orders:
         return 'No order added', 400
+    # check all products exist
+    #TODO loop
+    url = 'http://product-catalog/products/<TODO-id>'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        # Process the response data
+        print(data)
+    else:
+        print('Error:', response.status_code)
+
+    # check user exists
     orders.append(request.get_json())
     return orders, 201
 
