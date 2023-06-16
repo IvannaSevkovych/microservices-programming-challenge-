c74f5b38dcade9e93c6b5ba78fd6312c9edcc8e5
ivannaSevkovychItk <ivanna.sevkovych@itk-engineering.de>
2023-06-16 14:36:33 +0200

    Feat: add restart script

---
+++ restart.sh
@@ -0,0 +1,7 @@
+docker compose down
+docker rmi rest-user-management
+docker rmi rest-shipping
+docker rmi rest-order
+docker rmi rest-shipping_python
+docker rmi rest-product-catalog
+docker compose up
