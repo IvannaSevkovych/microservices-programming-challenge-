5a26fc86abe290674152f6b70dd6854ff55e3420
ivannaSevkovychItk <ivanna.sevkovych@itk-engineering.de>
2023-06-28 11:17:43 +0200

    Fix (azure only): define DB in product catalog service

--- product-catalog/helpers/db.js
+++ product-catalog/helpers/db.js
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.DB_HOST;
+const dbName = "test";
 let client;
 
 export async function connectToDatabase() {
@@ -9,5 +10,5 @@ export async function connectToDatabase() {
     await client.connect();
   }
 
-  return client.db(); // Return the database instance
+  return client.db(dbName); // Return the database instance
 }
